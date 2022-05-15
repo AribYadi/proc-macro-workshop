c2e9a86e1fd12de623de721828653d1f664e25dc
AribYadi <aribyadi@gmail.com>
2022-05-15 12:25:30 +0700

    feat(builder): pass test 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,49 @@
 use proc_macro::TokenStream;
+use quote::{
+  format_ident,
+  quote,
+};
 use syn::parse_macro_input;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-  let _input = parse_macro_input!(input as syn::DeriveInput);
-  TokenStream::new()
+  let input = parse_macro_input!(input as syn::DeriveInput);
+
+  let input_ident = input.ident;
+  let input_fields = match input.data {
+    syn::Data::Struct(syn::DataStruct { fields: syn::Fields::Named(fields), .. }) => fields,
+    _ => panic!("#[derive(Builder)] is only defined for structs"),
+  };
+  let input_field_idents =
+    input_fields.named.iter().map(|field| field.ident.as_ref().unwrap()).collect::<Vec<_>>();
+  let builder_ident = format_ident!("{}Builder", input_ident);
+  let builder_fields = get_builder_fields(&input_fields);
+
+  quote! {
+    pub struct #builder_ident {
+      #builder_fields
+    }
+
+    impl #input_ident {
+      pub fn builder() -> #builder_ident {
+        #builder_ident {
+          #(
+            #input_field_idents: None
+          ),*
+        }
+      }
+    }
+  }
+  .into()
+}
+
+fn get_builder_fields(input_fields: &syn::FieldsNamed) -> quote::__private::TokenStream {
+  let idents =
+    input_fields.named.iter().map(|field| field.ident.clone().unwrap()).collect::<Vec<_>>();
+  let types = input_fields.named.iter().map(|field| field.ty.clone()).collect::<Vec<_>>();
+  let vis = input_fields.named.iter().map(|field| field.vis.clone()).collect::<Vec<_>>();
+
+  quote! {
+    #(#vis #idents: Option<#types>),*
+  }
 }
