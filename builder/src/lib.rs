53ed08afc41045f8df3d0d6419f4f83c093eb3b0
AribYadi <aribyadi@gmail.com>
2022-05-15 11:44:08 +0700

    feat(builder): pass test 1

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,8 @@
 use proc_macro::TokenStream;
+use syn::parse_macro_input;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+  let _input = parse_macro_input!(input as syn::DeriveInput);
+  TokenStream::new()
 }
