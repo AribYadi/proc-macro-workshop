f00ed4ca4eb8c7286a25a62471a2d386522e21d7
AribYadi <aribyadi@gmail.com>
2022-05-17 16:30:33 +0700

    feat(debug): pass test 1

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::parse_macro_input;
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+  let input = parse_macro_input!(input as syn::DeriveInput);
+  let _ = input;
+  quote! {}.into()
 }
